bf8107c2f87c9e732682e3e4f268205866a9ef44
Douglas Schiavi <schiavi.douglas@gmail.com>
2018-06-26 19:50:30 -0300

    Corpo do que a gente tem so far

---
+++ Model/Main.java
@@ -0,0 +1,7 @@
+/* Main para chamar as classes do MMix */
+
+package Model;
+
+public class Main{
+    // public static void main(String[] args) {}
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # TrabsonPS
 Trabalho em grupo para disciplina de programação de Sistemas
+
+Com o diagrama de classes definido pelo .png
